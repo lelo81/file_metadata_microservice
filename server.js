a4e4c8cb3c792ba8f670cfdb4e64551452158871
lelo81 <le_lo@hotmail.com>
2016-04-27 18:19:31 +0000

    Latest changes

--- server.js
+++ server.js
@@ -1,31 +1,85 @@
 'use strict';
 
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
-
+var favicon = require('serve-favicon');
+var mongo = require('mongodb').MongoClient;
+var validator = require('validator');
+var shortid = require('shortid');
+var path = require('path');
 var app = express();
+
+app.use(favicon(__dirname + '/public/img/clementine_150.png'));
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
+app.get('/new/*', function(req, res) {
+  
+  var originalPath = req.originalUrl.substr(5,req.originalUrl.length);
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+  var host = req.protocol + '://' + req.get('host');
+  var doc = {};
+  
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+  if (validator.isURL(originalPath)){
+        doc.original_url = originalPath;
+        doc._id = shortid.generate();
+        
+        
+        mongo.connect(process.env.MONGO_URI, function(err, db) {
+        if (err) throw err;
+        var urls = db.collection('urls');
+        urls.insert(doc,function(err, data) {
+          if (err) throw err
+          doc.short_url = host + '/' + data.insertedIds[0];
+          delete doc._id;
+          db.close();
+          res.end(JSON.stringify(doc));
 
-app.use(passport.initialize());
-app.use(passport.session());
+      });
+  });
+        
+        
+        
+  } else {
+        res.end(JSON.stringify({"error":"Wrong url format, make sure you have a valid protocol and real site."}));
+  }
 
-routes(app, passport);
+  
+});
+
+app.get("/:id", function(req, res) {
+  
+  if (typeof req.params.id == 'string') {
+      
+      mongo.connect(process.env.MONGO_URI, function(err, db) {
+              if (err) throw err;
+              var urls = db.collection('urls');
+              urls.find({
+                "_id": req.params.id //o_id
+            }).toArray(function(err, documents) {
+                if (err) throw err
+                db.close();
+                
+                if (documents[0] == undefined) {
+                  res.end(JSON.stringify({"error":"This url is not in the database."}));
+                }
+                else {
+                  res.redirect(documents[0].original_url);
+                };
+            })
+      })  
+  }
+  else
+  {
+        res.sendFile(path.join(__dirname, 'public' + '/index.html'));
+  }
+});
+
+app.get('*', function(req, res) {
+
+    res.sendFile(path.join(__dirname, 'public' + '/index.html'));
+  
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
